73dffa854211f072c6bfa6ffa8600a0825e68234
Eugen Postea <epostea@gmail.com>
2025-02-25 10:33:39 -0500

    test6

--- sample6.sh
+++ sample6.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# Fetch the list of top CPU consumers on the host using system commands such as `top`
-top -b -n 1 | grep -E "%CPU|PID"
+# This script analyzes the data for a sudden increase compared to the previous 10 minutes
+
+# Get the current data
+current_data=$(cat /path/to/current/data)
+
+# Get the data from 10 minutes ago
+previous_data=$(cat /path/to/data/10minutes/ago)
+
+# Compare the current data with the previous data
+if [ "$current_data" -gt "$previous_data" ]; then
+  echo "There is a sudden increase in the data compared to the previous 10 minutes"
+else
+  echo "There is no sudden increase in the data compared to the previous 10 minutes"
+fi
