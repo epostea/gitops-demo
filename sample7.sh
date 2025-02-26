ea70d956dafb563bcdc525640fce07080da01c34
Eugen Postea <epostea@gmail.com>
2025-02-25 21:34:17 -0500

    test

--- sample7.sh
+++ sample7.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-# Use a monitoring tool to check the erroneous call rate metric
-# For example, using the curl command to check the metric from a web server:
-curl -X GET http://example.com/erroneous_call_rate
+
+# Use the command kubectl get pods to view the status of all Pods in the Deployment
+kubectl get pods
+
+# Look for any Pods with a status other than "Running"
+ pods=$(kubectl get pods | grep -v Running | awk '{print $1}')
+
+# If there are any, investigate the cause and address it accordingly
+if [ -n "$pods" ]; then
+  for pod in $pods; do
+    echo "Investigating pod $pod"
+    # Add your investigation and resolution steps here
+  done
+else
+  echo "All pods are running"
+fi
