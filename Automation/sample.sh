4450f4c582ad1d077142d35d5f6a694e9de3b5e1
Eugen Postea <epostea@gmail.com>
2025-04-07 15:12:52 -0400

    test

--- Automation/sample.sh
+++ Automation/sample.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Define the deployment name and desired number of replicas
-deployment_name="my-deployment"
-desired_replicas=5
+# Retrieve the YAML representation of the flagd-config ConfigMap
+flagd_config=$(kubectl get configmap flagd-config -o yaml)
 
-# Scale the deployment to match the desired number of replicas
-kubectl scale deployment $deployment_name --replicas=$desired_replicas
+# Extract the value of the adServiceHighCpu feature flag from the YAML
+ad_service_high_cpu=$(echo "$flagd_config" | grep adServiceHighCpu | awk '{print $2}')
+
+# Print the value of the adServiceHighCpu feature flag
+echo "The value of the adServiceHighCpu feature flag is: $ad_service_high_cpu"
