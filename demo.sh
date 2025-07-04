e21d35a064e8312ab459ad3b42ce20016aceaa6a
Eugen Postea <epostea@gmail.com>
2025-07-04 12:03:07 -0400

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,8 +3,8 @@
 # Main script
 
 # Download the required files
-curl -H 'Authorization: token $githubToken' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/epostea/gitops-demo/contents/python/main.py
-curl -H 'Authorization: token $githubToken' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/epostea/gitops-demo/contents/python/utils.py
+curl -H 'Authorization: token @@githubToken@@' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/epostea/gitops-demo/contents/python/main.py
+curl -H 'Authorization: token @@githubToken@@' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/epostea/gitops-demo/contents/python/utils.py
 
 # Run the python application
 python3 main.py
